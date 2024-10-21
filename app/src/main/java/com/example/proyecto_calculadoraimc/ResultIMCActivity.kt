c72e8649d87e13e547b5eba1ea83eef649a35e8f
SanchezLanchaCarlos <csanlan@g.educaand.es>
2024-10-21 11:36:50 +0200

    Diseño final

--- app/src/main/java/com/example/proyecto_calculadoraimc/ResultIMCActivity.kt
+++ app/src/main/java/com/example/proyecto_calculadoraimc/ResultIMCActivity.kt
@@ -5,15 +5,18 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ResultIMCActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        val boton = findViewById<Button>(R.id.fab_calcular)
+        setContentView(R.layout.activity_result_imcactivity)
+
+        val boton = findViewById<Button>(R.id.b_recalcular)
         boton.setOnClickListener {
-            val intent = Intent(this, ResultIMCActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
