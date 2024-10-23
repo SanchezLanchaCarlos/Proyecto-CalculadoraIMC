a5fa89a1b4db2483f3526fa7f2cddc3bfb73746f
SanchezLanchaCarlos <csanlan@g.educaand.es>
2024-10-23 12:28:06 +0200

    Lógica añadida

--- app/src/main/java/com/example/proyecto_calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_calculadoraimc/MainActivity.kt
@@ -1,19 +1,76 @@
 package com.example.proyecto_calculadoraimc
 
 import android.content.Intent
+import android.icu.text.DecimalFormat
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
 
 class MainActivity : AppCompatActivity() {
+    lateinit var cv_hombre: CardView
+    lateinit var cv_mujer: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val boton = findViewById<Button>(R.id.fab_calcular)
-        boton.setOnClickListener {
+
+        val rsHeight = findViewById<RangeSlider>(R.id.rs_altura)
+        cv_hombre = findViewById(R.id.cv_hombre)
+        cv_mujer = findViewById(R.id.cv_mujer)
+        var currentHeight: Int
+        var currentWeight = 60
+        var currentAge = 30
+        var altura = 0.0
+        var tvHeight = findViewById<TextView>(R.id.tv_altura)
+        var tvPeso = findViewById<TextView>(R.id.tv_peso)
+        var tvEdad = findViewById<TextView>(R.id.tv_edad)
+
+        rsHeight.addOnChangeListener {_, value, _ ->
+            val df = DecimalFormat("#.##")
+            currentHeight = df.format(value).toInt()
+            altura = currentHeight/100.0
+            tvHeight.text = currentHeight.toString() + " cm"
+        }
+        val botonPesoMas = findViewById<FloatingActionButton>(R.id.fab_peso_mas)
+        botonPesoMas.setOnClickListener {
+            currentWeight += 1
+            tvPeso.text = currentWeight.toString()
+        }
+        val botonPesoMenos = findViewById<FloatingActionButton>(R.id.fab_peso_menos)
+        botonPesoMenos.setOnClickListener {
+            currentWeight -= 1
+            tvPeso.text = currentWeight.toString()
+        }
+        val botonEdadMas = findViewById<FloatingActionButton>(R.id.fab_edad_mas)
+        botonEdadMas.setOnClickListener {
+            currentAge += 1
+            tvEdad.text = currentAge.toString()
+        }
+        val botonEdadMenos = findViewById<FloatingActionButton>(R.id.fab_edad_menos)
+        botonEdadMenos.setOnClickListener {
+            currentAge -= 1
+            tvEdad.text = currentAge.toString()
+        }
+        cv_hombre.setOnClickListener {
+            cv_hombre.setCardBackgroundColor(ContextCompat.getColor(applicationContext,R.color.fondo_cvs))
+            cv_mujer.setCardBackgroundColor(ContextCompat.getColor(applicationContext,R.color.fondo_csv_oscuro))
+        }
+        cv_mujer.setOnClickListener {
+            cv_hombre.setCardBackgroundColor(ContextCompat.getColor(applicationContext,R.color.fondo_csv_oscuro))
+            cv_mujer.setCardBackgroundColor(ContextCompat.getColor(applicationContext,R.color.fondo_cvs))
+        }
+        val botonCalcular = findViewById<Button>(R.id.fab_calcular)
+        botonCalcular.setOnClickListener {
+            val imc = currentWeight/(altura*altura)
             val intent = Intent(this, ResultIMCActivity::class.java)
+            intent.putExtra("IMC", imc)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/proyecto_calculadoraimc/ResultIMCActivity.kt
+++ app/src/main/java/com/example/proyecto_calculadoraimc/ResultIMCActivity.kt
@@ -3,10 +3,9 @@ package com.example.proyecto_calculadoraimc
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ResultIMCActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +13,38 @@ class ResultIMCActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_result_imcactivity)
 
+        val tvImc = findViewById<TextView>(R.id.tv_imc)
+        val tvTitulo = findViewById<TextView>(R.id.tv_titulo)
+        val tvDescripcion = findViewById<TextView>(R.id.tv_descripcion)
+        val imc = intent.getDoubleExtra("IMC", 0.0)
+        tvImc.text = String.format("%.2f", imc)
+
+        when {
+            imc in 0.0..18.49 -> {
+                tvTitulo.text = getString(R.string.bajo_peso)
+                tvTitulo.setTextColor(getColor(R.color.amarillo))
+                tvDescripcion.text = getString(R.string.bajoPeso_ext)
+            }
+            imc in 18.5..24.99 ->{
+                tvTitulo.text = getString(R.string.normal)
+                tvTitulo.setTextColor(getColor(R.color.verde))
+                tvDescripcion.text = getString(R.string.normal_ext)
+            }
+            imc in 24.5..29.99 ->{
+                tvTitulo.text = getString(R.string.sobrepeso)
+                tvTitulo.setTextColor(getColor(R.color.naranja))
+                tvDescripcion.text = getString(R.string.sobrepeso_ext)
+            }
+            imc >= 30 -> {
+                tvTitulo.text = getString(R.string.obesidad)
+                tvTitulo.setTextColor(getColor(R.color.rojo))
+                tvDescripcion.text = getString(R.string.obesidad_ext)
+            }
+        }
+
         val boton = findViewById<Button>(R.id.b_recalcular)
         boton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 }
